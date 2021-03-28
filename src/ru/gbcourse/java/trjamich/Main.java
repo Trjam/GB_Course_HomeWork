69588e079ca68924fb629fb6d8a5c2b9383eb6b4
Trjam <alien.trjam@gmail.com>
2021-03-28 23:38:41 +0300

    Java_lvl2_Lesson_5

--- src/ru/gbcourse/java/trjamich/Main.java
+++ src/ru/gbcourse/java/trjamich/Main.java
@@ -1,7 +1,22 @@
 package ru.gbcourse.java.trjamich;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+
+
+        MyArray.fillArray(arr);
+        long t = System.currentTimeMillis();
+        SingleThread.calculate (arr, 0);
+        System.out.println(System.currentTimeMillis() - t);
+
+
+        MyArray.fillArray(arr);
+        t = System.currentTimeMillis();
+        DoubleThread.calculate(arr, h);
+        System.out.println(System.currentTimeMillis() - t);
     }
 }
---
+++ src/ru/gbcourse/java/trjamich/MyArray.java
@@ -0,0 +1,10 @@
+package ru.gbcourse.java.trjamich;
+
+public class MyArray {
+
+    public static void fillArray(float[] arr) {
+        for (float a : arr) {
+            a=1;
+        }
+    }
+}
